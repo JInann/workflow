1c9afaddf862f3dc1e9b9edd55f053f21dbb07e7
jiyanbin <jiyanbin@021.com>
2024-01-08 16:53:26 +0800

    dev deploy

--- dev.deploy.js
+++ dev.deploy.js
@@ -1,1 +1,28 @@
-console.log("deploy dev");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const OSS = require("ali-oss");
+const fs = require("fs");
+const readDir = (path) => {
+  let arr = [];
+  let temp = fs.readdirSync(path);
+  temp.forEach((v) => {
+    if (fs.statSync(path + "/" + v).isDirectory()) {
+      arr.push(...readDir(path + "/" + v));
+    } else {
+      arr.push(path + "/" + v);
+    }
+  });
+  return arr;
+};
+
+let client = new OSS({
+  region: "oss-cn-hongkong",
+  accessKeyId: process.env.ACCESSKEYID,
+  accessKeySecret: process.env.ACCESSKEYSECRET,
+});
+
+client.useBucket("jyb-site");
+readDir("./pre").forEach((file) => {
+  client.put("workflow/" + file.split("./dist/")[1], file).then(() => {
+    console.log("上传成功:", file);
+  });
+});
