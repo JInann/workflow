32defe09192277c190a18dbaa10794518082c020
jiyanbin <jiyanbin@021.com>
2024-01-08 16:37:01 +0800

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
-import legacy from "@vitejs/plugin-legacy";
 export default defineConfig(async ({ mode }) => {
   return {
     root: resolve(__dirname, "src/pages/index/"),
@@ -13,13 +12,7 @@ export default defineConfig(async ({ mode }) => {
     },
     assetsInclude: ["**/*.svga"],
     envDir: resolve(__dirname),
-    plugins: [
-      vue(),
-      // 兼容插件
-      legacy({
-        targets: ["defaults", "iOS >= 10"],
-      }),
-    ],
+    plugins: [vue()],
     build: {
       target: "es2015",
       rollupOptions: {
